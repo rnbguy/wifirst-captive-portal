462fba272764de0160a24da6ef1ddb270fe79d54
Ranadeep Biswas <mail@rnbguy.at>
2022-10-30 13:48:19 +0100

    shellharden

--- wifirst.sh
+++ wifirst.sh
@@ -12,9 +12,21 @@ CURR_URL=$(curl 'https://connect.wifirst.net/?perform=true' \
 for N_LOOP in {1..5}; do
   echo "URL: ${CURR_URL}"
   case "${CURR_URL}" in
+    https://connect.wifirst.net/?perform=true)
+      echo "Trying to auto-log."
+      CURR_URL=$(curl 'https://apps.wifirst.net/?redirected=true' \
+        -K curl.conf \
+        -o "${AUTH_TOKEN_FILE}" \
+        -w %{url_effective})
+      echo "URL: ${CURR_URL}"
+      ;;
+    https://apps.wifirst.net/?redirected=true)
+      CURR_URL="https://selfcare.wifirst.net/sessions/new"
+      ;;
     https://selfcare.wifirst.net/sessions/new*)
       . cred.sh
       AUTH_TOKEN=$(grep -oP '(?<=<input name="authenticity_token" type="hidden" value=")[^"]*(?=" /></div>)' "${AUTH_TOKEN_FILE}")
+      echo "${AUTH_TOKEN}"
       CURR_URL=$(curl -X POST 'https://selfcare.wifirst.net/sessions' \
         -K curl.conf \
         --data-urlencode "utf8=✓" \
